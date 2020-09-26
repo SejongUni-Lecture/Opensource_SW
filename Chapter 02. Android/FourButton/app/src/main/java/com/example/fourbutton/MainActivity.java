a68dc67a01c925feeab8836e9ff406800713cb3e
ameliacode <17011663@sju.ac.kr>
2020-09-26 21:11:04 +0900

    Update Android

---
+++ Chapter 02. Android/FourButton/app/src/main/java/com/example/fourbutton/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.fourbutton;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button1, button2, button3, button4;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_launcher);
+
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setBackgroundColor(Color.GRAY);
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setBackgroundColor(Color.GREEN);
+
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setBackgroundColor(Color.RED);
+
+        button4 = (Button) findViewById(R.id.button4);
+        button4.setBackgroundColor(Color.YELLOW);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.nate.com"));
+                startActivity(mIntent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:/911"));
+                startActivity(mIntent);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media"));
+                startActivity(mIntent);
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
